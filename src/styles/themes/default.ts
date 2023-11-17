6e580aa5702211201a1976a43c78774fd0eeaf49
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-11-17 16:32:57 -0300

    Configurando ESLint

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,14 +1,14 @@
 export const defaultTheme = {
   // Produto
   'yellow-dark': '#C47F17',
-  'yellow': '#DBAC2C',
+  yellow: '#DBAC2C',
   'yellow-light': '#F1E9C9',
   'purple-dark': '#4B2995',
-  'purple': '#8047F8',
+  purple: '#8047F8',
   'purple-light': '#EBE5F9',
 
   // Base
-  'background': '#333',
+  background: '#333',
   'base-title': '#272221',
   'base-subtitle': '#403937',
   'base-text': '#574F4D',
@@ -17,5 +17,5 @@ export const defaultTheme = {
   'base-button': '#E6E5E5',
   'base-input': '#EDEDED',
   'base-card': '#F3F2F2',
-  'white': '#FFFFFF'
+  white: '#FFFFFF',
 }
